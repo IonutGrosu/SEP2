0b17118f02cdf54b5718eab00e29618e720ecc4b
MariAsenova <marigithuba1>
2020-12-01 13:16:04 +0100

    JDBCController connection with the database

--- WarehouseSystem/src/warehouse/client/core/ViewModelFactory.java
+++ WarehouseSystem/src/warehouse/client/core/ViewModelFactory.java
@@ -2,16 +2,20 @@ package warehouse.client.core;
 
 import warehouse.client.views.loginview.LoginViewModel;
 
-public class ViewModelFactory {
-    private ModelFactory modelFactory;
-    private LoginViewModel loginViewModel;
+public class ViewModelFactory
+{
+  private ModelFactory modelFactory;
+  private LoginViewModel loginViewModel;
 
-    public ViewModelFactory(ModelFactory modelFactory) {
-        this.modelFactory = modelFactory;
-    }
+  public ViewModelFactory(ModelFactory modelFactory)
+  {
+    this.modelFactory = modelFactory;
+  }
 
-    public LoginViewModel getLoginViewModel() {
-        if (loginViewModel == null) loginViewModel = new LoginViewModel(modelFactory.getWarehouseModel());
-        return loginViewModel;
-    }
+  public LoginViewModel getLoginViewModel()
+  {
+    if (loginViewModel == null)
+      loginViewModel = new LoginViewModel(modelFactory.getWarehouseModel());
+    return loginViewModel;
+  }
 }
--- WarehouseSystem/src/warehouse/persistence/JDBCController.java
+++ WarehouseSystem/src/warehouse/persistence/JDBCController.java
@@ -1,4 +1,32 @@
 package warehouse.persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCController {
+  private static JDBCController instance;
+
+  private JDBCController()
+  {
+  }
+
+  public static JDBCController getInstance()
+  {
+    if (instance == null)
+    {
+      instance = new JDBCController();
+    }
+    return instance;
+  }
+
+  public Connection getConnection() throws SQLException
+  {
+    String url = "hattie.db.elephantsql.com:5432/wzzxaats?currentSchema=SEP2_Database";
+    String username = "postgres";
+    String password = "9533";
+    return DriverManager.getConnection(url, username, password);
+  }
+
 }
+
