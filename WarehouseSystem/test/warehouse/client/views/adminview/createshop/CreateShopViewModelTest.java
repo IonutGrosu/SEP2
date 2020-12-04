bc0691f77a187b9c0ade976b9186b30da263035e
IonutGrosu <ionutflorentin.grosu@gmail.com>
2020-12-04 13:39:13 +0100

    FXML and MVVM (admin adding shop view) -- added some tests

---
+++ WarehouseSystem/test/warehouse/client/views/adminview/createshop/CreateShopViewModelTest.java
@@ -0,0 +1,24 @@
+package warehouse.client.views.adminview.createshop;
+
+import org.junit.jupiter.api.Test;
+import warehouse.client.model.WarehouseModel;
+import warehouse.client.model.WarehouseModelImpl;
+import warehouse.client.networking.Client;
+import warehouse.client.networking.RMIClientImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateShopViewModelTest {
+    Client client = new RMIClientImpl();
+    WarehouseModel model = new WarehouseModelImpl(client);
+
+    @Test
+    public void creatingViewModel(){
+        CreateShopViewModel viewModel = new CreateShopViewModel(model);
+    }
+
+    @Test
+    public void creatingViewController(){
+        CreateShopViewController controller = new CreateShopViewController();
+    }
+}
